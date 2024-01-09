a14bfa16de8b7947a3766bbcfafec0d7b61fac5b
Duyycoder <duysusi872gmail.com>
2024-01-09 20:59:21 +0700

    poolling use

--- Assets/_FlappyBird/Script/Player.cs
+++ Assets/_FlappyBird/Script/Player.cs
@@ -1,18 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using GameTool;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]private Rigidbody2D yb;
 
-    private float jumpForce = 3f;
+    private float jumpForce = 10f;
+    public float cooldown;
+    public float timeShoot = 0.5f;
+
+    public GameObject prefab;
     // Start is called before the first frame update
     void Start()
     {
         // yb.AddForce(new Vector2(x:0 ,y:100));
         yb.velocity = new Vector2(0 ,2 );
+        cooldown = timeShoot;
     }
 
     // Update is called once per frame
@@ -22,8 +29,13 @@ public class Player : MonoBehaviour
         {
             yb.velocity = new Vector2(0, jumpForce);
         }
-        
-        
+
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0.0f)
+        {
+            PoolingManager.Instance.GetObject(NamePrefabPool.Bullet,parent:null , transform.position).Disable(1);
+            cooldown = timeShoot;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -36,6 +48,9 @@ public class Player : MonoBehaviour
         {
             Debug.Log(message:"Collided with block");
         }
+
+        // Instantiate(prefab, new Vector3(0f, -1.4f, 0f), quaternion.identity);
+        // Destroy(other.gameObject);
     }
 
     // private void OnTriggerEnter2D(Collider2D other)
